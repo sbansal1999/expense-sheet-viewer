be1fdb82371cd8c1ae63286d3cb29630f482031b
Satyam Bansal <satbansa@cisco.com>
2024-02-18 19:19:51 +0530

    Add ShadCN.

--- src/components/LatestExpense.tsx
+++ src/components/LatestExpense.tsx
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { Alert, AlertDescription, AlertTitle } from "./ui/alert";
+import { ExclamationTriangleIcon } from "@radix-ui/react-icons";
 
 const inter = Inter({ subsets: ["latin"] });
 const LATEST_EXPENSE_KEY = "latest_expense";
@@ -16,8 +18,20 @@ export default function LatestExpense() {
 	}
 
 	if (isError) {
-		return "error...";
+		return <ErrorAlert />;
 	}
 
 	return <main className={`${inter.className}`}>{JSON.stringify(data)}</main>;
 }
+
+const ErrorAlert = () => {
+	return (
+		<Alert variant={"destructive"}>
+			<ExclamationTriangleIcon className="h-4 w-4" />
+			<AlertTitle>Error</AlertTitle>
+			<AlertDescription>
+				Something went wrong. Check the logs...
+			</AlertDescription>
+		</Alert>
+	);
+};
