0a42004f59a40eb64ed7a3972824f63f8c318c67
Satyam Bansal <satbansa@cisco.com>
2024-02-25 19:10:05 +0530

    Send first row as schema data through the API.

--- src/pages/api/sheets.ts
+++ src/pages/api/sheets.ts
@@ -4,34 +4,38 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 export const SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-	const auth = new google.auth.GoogleAuth({
-        scopes: SCOPES,
-        projectId: process.env.PROJECT_ID,
-        credentials:{
-            private_key: process.env.PRIVATE_KEY,
-            client_email: process.env.CLIENT_EMAIL
-        }
-    });
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const auth = new google.auth.GoogleAuth({
+    scopes: SCOPES,
+    projectId: process.env.PROJECT_ID,
+    credentials: {
+      private_key: process.env.PRIVATE_KEY,
+      client_email: process.env.CLIENT_EMAIL,
+    },
+  });
 
-	const sheets = google.sheets({ version: "v4", auth });
+  const sheets = google.sheets({ version: "v4", auth });
 
-	const range = "'Form Responses 1'";
+  const range = "'Form Responses 1'";
 
-	const response = await sheets.spreadsheets.values.get({
-		spreadsheetId: process.env.SPREADSHEET_ID,
-		range,
-	});
+  const response = await sheets.spreadsheets.values.get({
+    spreadsheetId: process.env.SPREADSHEET_ID,
+    range,
+  });
 
-	const data = response.data.values;
+  const data = response.data.values;
 
-	if (!data) {
-		res.send("empty_sheet");
-		return;
-	}
+  if (!data) {
+    res.send("empty_sheet");
+    return;
+  }
 
-	const lastRow = data[data.length - 1];
+  const firstRow = data[0];
+  const lastRow = data[data.length - 1];
 
-	console.log(lastRow);
-	res.send(lastRow);
+  console.log(lastRow);
+  res.send({ schema: firstRow, lastRow });
 }
