4919fc219ccdefa0167cd0ce5c3406773cca8f51
Satyam Bansal <satbansa@cisco.com>
2024-03-12 12:41:41 +0530

    Add Vercel Speedinsights.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const queryClient = new QueryClient();
 
@@ -12,6 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
 			<Component {...pageProps} />
 			<ReactQueryDevtools initialIsOpen={false} />
 			<Analytics/>
+			<SpeedInsights/>
 		</QueryClientProvider>
 	);
 }
