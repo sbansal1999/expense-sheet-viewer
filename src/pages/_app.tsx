32545118d7b53853eee0709e6c56a16d5b307c1d
Satyam Bansal <satbansa@cisco.com>
2024-03-15 23:05:20 +0530

    Add Dark Theme and set it as default.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,16 +4,19 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { ThemeProvider } from "@/components/theme_provider";
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<QueryClientProvider client={queryClient}>
-			<Component {...pageProps} />
-			<ReactQueryDevtools initialIsOpen={false} />
-			<Analytics/>
-			<SpeedInsights/>
+			<ThemeProvider attribute="class" defaultTheme="dark">
+				<Component {...pageProps} />
+				<ReactQueryDevtools initialIsOpen={false} />
+				<Analytics/>
+				<SpeedInsights/>
+			</ThemeProvider>
 		</QueryClientProvider>
 	);
 }
