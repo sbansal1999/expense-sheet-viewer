371e91cd8fa339750fe2ae0c7ef73fe862af1aea
Satyam Bansal <satbansa@cisco.com>
2024-04-20 23:23:13 +0530

    util: Add util to help users copying credentials to .env

---
+++ env-util.js
@@ -0,0 +1,29 @@
+function populateENV() {
+  const fs = require("fs");
+  let secrets;
+
+  try {
+    secrets = require("./secrets.json");
+  } catch (err) {
+    console.log("secrets.json file not found.");
+    return;
+  }
+
+  const { project_id, private_key, client_email } = secrets;
+
+  const envData = {
+    PROJECT_ID: project_id,
+    PRIVATE_KEY: private_key.replace(/\n/g, "\\n"),
+    CLIENT_EMAIL: client_email,
+    SPREADSHEET_ID: "",
+  };
+
+  const envContent = Object.entries(envData)
+    .map(([key, value]) => `${key}="${value}"`)
+    .join("\n");
+
+  fs.writeFileSync(".env", envContent);
+  console.log(".env file has been successfully populated.");
+}
+
+populateENV();
