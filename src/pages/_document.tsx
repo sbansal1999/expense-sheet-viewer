19baa380e9abca9934c029545e1bbd0b917b4232
Satyam Bansal <sbansal1999@gmail.com>
2026-01-01 15:36:54 +0530

    Fix databuddy script.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,19 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
+        <script
+          src="https://cdn.databuddy.cc/databuddy.js"
+          data-client-id="84de170a-6dce-4801-889f-6dbdcf821289"
+          data-track-hash-changes="true"
+          data-track-attributes="true"
+          data-track-outgoing-links="true"
+          data-track-interactions="true"
+          data-track-scroll-depth="true"
+          data-track-web-vitals="true"
+          data-track-errors="true"
+          crossOrigin="anonymous"
+          async
+        ></script>
         <Main />
         <NextScript />
       </body>
